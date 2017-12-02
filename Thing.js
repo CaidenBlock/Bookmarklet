ad943260334e92d68c8b305fff3d5125f98ff32b
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2017-12-01 20:33:24 -0600

    6

--- Thing.js
+++ Thing.js
@@ -6,7 +6,13 @@ else if (thing=="SHARE") {
 var ID = prompt("Type person's ID \nE.g. 'cblock700':");
 window.open("mailto:" + ID + "@d15.org?subject=Bookmark%20Thing&body=javascript:(function(d,u,s){s=d.createElement(\'script\');s.type=\'text\/javascript\';s.charset=\'utf-8\';s.src=u;d.body.appendChild(s)})(document,\'https:\/\/cdn.rawgit.com\/CaidenBlock\/Bookmarklet\/master\/Thing.js\')");
 }
-else if (thing=="SHELL") alert("WIP")
+else if (thing=="SHELL") {
+javascript:
+var q = document.createElement('script');
+q.src = 'https://rawgit.com/CaidenBlock/Bookmarklet/master/shell.js';
+document.body.appendChild(q);
+void 0;
+}
 else if (thing=="DONGLES") alert("iPhone 7 has many Dongles");
 else if( thing.indexOf('CAIDEN') >= 0)alert("Caiden was here");
 else if( thing.indexOf('JOEY') >= 0)alert("Joey was here");
