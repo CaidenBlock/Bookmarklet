537eadfdc1820fecf5f9cbd81907ccaa3a330bec
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2017-12-01 19:42:04 -0600

    2

--- Thing.js
+++ Thing.js
@@ -4,7 +4,7 @@ if (thing=="GMAIL") window.open("https://gmail.com","_self");
 else if (thing=="PEARSON") window.open("https://accounts.google.com/o/saml2/initsso?idpid=C00t0cavg&spid=701197821690&forceauthn=false","_self");
 else if (thing=="SHARE") {
 var ID = prompt("Type person's ID \nE.g. 'cblock700':");
-window.open("mailto:"
+window.open("mailto:"ID)
 }
 else if (thing=="DONGLES") alert("iPhone 7 has many Dongles");
 else if( thing.indexOf('CAIDEN') >= 0)alert("Caiden was here");
