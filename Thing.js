9d526cad3b87ba0f30aaf3b6b1ea6ae9dbd7f7ac
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2017-12-01 21:34:16 -0600

    7

--- Thing.js
+++ Thing.js
@@ -1,15 +1,18 @@
 var ting = prompt("Thing:");
+var censor = ["DICK", "FUCK", "SHIT", "ASS", "DICK", "PENIS"];
 var thing = ting.toUpperCase();
 if (thing=="GMAIL") window.open("https://gmail.com","_self");
 else if (thing=="PEARSON") window.open("https://accounts.google.com/o/saml2/initsso?idpid=C00t0cavg&spid=701197821690&forceauthn=false","_self");
 else if (thing=="SHARE") {
 var ID = prompt("Type person's ID \nE.g. 'cblock700':");
-window.open("mailto:" + ID + "@d15.org?subject=Bookmark%20Thing&body=javascript:(function(d,u,s){s=d.createElement(\'script\');s.type=\'text\/javascript\';s.charset=\'utf-8\';s.src=u;d.body.appendChild(s)})(document,\'https:\/\/cdn.rawgit.com\/CaidenBlock\/Bookmarklet\/master\/Thing.js\')");
+var IDlc = ID.toLowerCase();
+if( IDlc=="cblock700") alert("That was the example...\nWhy would you even put me? I made this!");
+else window.open("mailto:" + ID + "@d15.org?subject=Bookmark%20Thing&body=javascript:(function(d,u,s){s=d.createElement(\'script\');s.type=\'text\/javascript\';s.charset=\'utf-8\';s.src=u;d.body.appendChild(s)})(document,\'https:\/\/cdn.rawgit.com\/CaidenBlock\/Bookmarklet\/master\/Thing.js\')");
 }
-else if (thing=="Easter Egg") window.open("https:en.wikipedia.org/wiki/Easter_egg_\(media\)");
-else if (thing=="DONGLES") alert("iPhone 7 has many Dongles");
-else if( thing.indexOf('CAIDEN') >= 0)alert("Caiden was here");
-else if( thing.indexOf('JOEY') >= 0)alert("Joey was here");
-var censor = ["DICK", "FUCK", "SHIT", "ASS", "DICK", "PENIS"];
-else if( thing.indexOf(censor) >= 0)alert("Censored");
+else if (thing=="Easter Egg") window.open("https:en.wikipedia.org/wiki/Easter_egg_(media)");
+else if (thing=="Creator") alert("Caiden Block made this!");
+else if( thing.indexOf('CAIDEN') >= 0) alert("Caiden was here");
+else if( thing.indexOf('JOEY') >= 0) alert("Joey was here");
+else if(censor.some(v => v===thing))alert("Censored");
 else alert(ting);
+
