56d8773c57f45597eaf1d50b0da088f81390f2ed
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2018-05-21 15:48:47 -0500

    Update Thing.js

--- Thing.js
+++ Thing.js
@@ -34,10 +34,10 @@ else if (thing=="INSPECT") {
 }
 else if (thing=="USERDEFINED") { 
   var thinglist = document.createElement('text');
-  thinglist.setAttribute('src', 'https://rawgit.com/CaidenBlock/Bookmarklet/master/ThingEntries.txt');
+  thinglist.setAttribute('src', 'https://rawgit.com/CaidenBlock/Bookmarklet/master/ThingEntries.js');
   document.body.appendChild(thinglist);
   alert(thinglist);
-  //void(thinglist);
+  void(thinglist);
 }
 else if (thing=="EASTER EGG") window.open("https:en.wikipedia.org/wiki/Easter_egg_(media)");
 else if (thing=="CREATOR") alert("Caiden Block made this!");
