29a2af1f6451f48ce3ef27bc19f0e3f0b001ee19
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2018-05-22 17:38:37 -0500

    Update Thing.js

--- Thing.js
+++ Thing.js
@@ -34,7 +34,7 @@ else if (thing=="INSPECT") {
 }
 else if (thing=="USERDEFINED") { 
   var thinglist = document.createElement('script');
-  thinglist.setAttribute('src','https://rawgit.com/CaidenBlock/Bookmarklet/master/ThingEntries.js');
+  thinglist.src = 'https://rawgit.com/CaidenBlock/Bookmarklet/master/ThingEntries.js';
   document.body.appendChild(thinglist);
   alert(thinglist.entries[1]);
 }
