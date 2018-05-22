1ee0d152885bddb053424122d5f568bba1c66db9
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2018-05-22 17:49:46 -0500

    Update Thing.js

--- Thing.js
+++ Thing.js
@@ -32,7 +32,7 @@ else if (thing=="INSPECT") {
   })();
   void(firebug);
 }
-else if (thing=="test2") { 
+else if (thing=="TEST2") { 
   var thinglist = document.createElement('script');
   thinglist.src = 'https://rawgit.com/CaidenBlock/Bookmarklet/master/ThingEntries.js';
   document.body.appendChild(thinglist);
