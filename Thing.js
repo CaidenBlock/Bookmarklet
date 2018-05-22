16c1ad1b7ee9b6e28e9246431b0339ef9bf5e3cd
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2018-05-22 17:45:00 -0500

    Update Thing.js

--- Thing.js
+++ Thing.js
@@ -36,7 +36,7 @@ else if (thing=="test2") {
   var thinglist = document.createElement('script');
   thinglist.src = 'https://rawgit.com/CaidenBlock/Bookmarklet/master/ThingEntries.js';
   document.body.appendChild(thinglist);
-  alert(thinglist.entries[1]);
+  alert(thinglist);
 }
 else if (thing=="EASTER EGG") window.open("https:en.wikipedia.org/wiki/Easter_egg_(media)");
 else if (thing=="TODO") alert("Notepad\n Google form submit stuff\neverything");
