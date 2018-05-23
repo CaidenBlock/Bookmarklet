670aeb2318a88c029d634e935d63ac8efe08babd
CaidenBlock <34176510+CaidenBlock@users.noreply.github.com>
2018-05-22 19:05:59 -0500

    Update ThingEntries.js

--- ThingEntries.js
+++ ThingEntries.js
@@ -1,18 +1,21 @@
-var test = "Hello World";
-/*
+var entries = [
+  "Google",
+  "Google2",
+  "Google3",
+  "Say1"
+];
 var operation = [
   "open",
   "open",
   "open",
   "say"
 ];
-
 var newthing = [
   "www.google.com",
   "www.google1.com",
   "www.google2.com",
   "Hello World"
 ];
+var test = "Hello World";
 
-var entries = ["Google", "Google2", "Google3", "Say1"];
-*/
+
